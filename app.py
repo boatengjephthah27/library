c434b510b505d837f94512e0d585c710fc359f16
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-06-03 17:20:02 +0000

    .

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from tkinter import *
+import sqlite3
+
+conn = sqlite3.connect("bookstore.db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS bookshelf (book TEXT, quantity INT, price REAL)" )
+conn.commit()
+conn.close()
 
 
+def add(book, quantity, price):
+    conn = sqlite3.connect("bookstore.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO bookshelf VALUES (?,?,?)", (book, quantity, price))
+    conn.commit()
+    conn.close()
 
+m = input("book name: --  ")
+n = int(input("book quantity: --  "))
+v = float(input("book price: --  "))
 
+add(m,n,v)
 
+# def ()
 
 
 

08cdbcbf9403e763906a1c3cd607e3e43b73cc69
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-01 17:07:14 +0000

    venv created

--- app.py
+++ app.py
@@ -16,33 +16,35 @@ page_label = Label(app, text='Page At').grid(row=1, column=2, sticky=NSEW)
 # creating entry widgets
 t = StringVar()
 t_entry = Entry(app, borderwidth=3, textvariable=t).grid(row=0,column=1, sticky=NSEW)
+
 y = StringVar()
 y_entry = Entry(app, borderwidth=3, textvariable=y).grid(row=1,column=1, sticky=NSEW)
+
 a = StringVar()
 a_entry = Entry(app, borderwidth=3, textvariable=a).grid(row=0,column=3, sticky=NSEW)
+
 p = StringVar()
 p_entry = Entry(app, borderwidth=3, textvariable=p).grid(row=1,column=3, sticky=NSEW)
 
+# creating a listbox
+t_lbox = Listbox(app, width=28).grid(padx=3, pady=5, row=2, column=0, columnspan=2, rowspan=6, sticky=NSEW)
 
+# creating a scrollbar
+svbar = Scrollbar(app).grid(row=2, column=2, rowspan=6, sticky=NSEW)
+# shbar = Scrollbar(app, orient=HORIZONTAL).grid(row=8, column=0, columnspan=3, sticky=NSEW)
 
+# configuring the listbox and scrollbar
+# t_lbox.tix_configure(yscrollcommand=svbar.set, xscrollommand=shbar.set)
+# svbar.tix_configure(command=t_lbox.yview)  
+# shbar.config(command=t_lbox.xview)
 
 
 # creating functions
 def show():
     for row in appbackend.print_it():
         t_lbox.insert(END, row)
-
-# creating a listbox
-t_lbox = Listbox(app, width=28).grid(padx=3, pady=5, row=2, column=0, columnspan=2, rowspan=6, sticky=NSEW)
-
-# creating a scrollbar
-svbar = Scrollbar(app, orient=VERTICAL).grid(row=2, column=2, rowspan=6, sticky=NSEW)
-shbar = Scrollbar(app, orient=HORIZONTAL).grid(row=8, column=0, columnspan=3, sticky=NSEW)
-
-# configuring the listbox and scrollbar
-# t_lbox.config(yscrollcommand=svbar.set, xscrollommand=shbar.set)
-# svbar.config(command=t_lbox.yview)
-# shbar.config(command=t_lbox.xview)
+        
+        
 
 # creating buttons
 viewb = Button(app, text='View', width=15, command=show,).grid(row=2,column=3, sticky=NSEW)
